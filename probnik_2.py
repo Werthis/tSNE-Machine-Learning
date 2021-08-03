6fd44629b48f2b58c4011ac9ff792cbe5e9a7af5
Janko Walski <lukaszstawecki@gmail.com>
2021-08-03 18:47:39 +0200

    VIP #1

--- probnik_2.py
+++ probnik_2.py
@@ -1,28 +1,24 @@
 from csv import reader
-
-lista_rows = []
-
-
-# URL_DATASET = r'dialanine-300K.csv'
-# cov_19_data_frame = pd.read_csv(URL_DATASET)
-
-# for rows in cov_19_data_frame:
-#     for row in rows:
-#         lista_rows.append(row)
+import numpy as np
 
 
 
-# print(cov_19_data_frame)
-# print(lista_rows)
-
-
-# open file in read mode
 with open('probnik.csv', 'r') as data_base:
-    # pass the file object to reader() to get the reader object
+    lista_var = []
+
     csv_reader = reader(data_base)
-    # Iterate over each row in the csv using reader object
     for row in csv_reader:
-        # row variable is a list that represents a row in csv
-        for i in row[1:]:
-            print(i.split(" "))
-            # print(row)
+        for i in row:
+            single_number_list = i.split(" ")
+            # print(single_number_list)
+            only_distances_list = single_number_list[6:]
+            arr = np.array(only_distances_list)
+            print(arr)
+            # variance = np.var(arr, ddof=1)
+            # print(variance)
+            # # lista_var.append(variance)
+            # print(lista_var)
+            for j in single_number_list[5:]:
+                pass
+                
+    print(len(i.split(" ")))
--- tsne.py
+++ tsne.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_digits
+# from sklearn.datasets import load_digits
 
 # digits = load_digits()
 
@@ -11,19 +11,31 @@ class TSNE():
     def __init__(self):
         pass
 
-    def open_deta_file(self):
+    def open_data_file(self):
         self.data_file = open("dialanine-300K.data", "r")
-        self.data_file.read()
-        # print(self.data_file.head)
+        self.data_read = self.data_file.read()
+        print(self.data_read[5])
+
+    def get_one_line(self):
+        for line in self.data_read[5:]:
+            pass
+
+    def put_data_into_txt(self):
+        self.file_name = 'data_300K_txt.txt'
+        self.world_map_file_1 = open(self.file_name, 'w')
+        self.world_map_file_1.write(str(self.data_read))
+        self.world_map_file_1.close()
+
 
 
     def close_data_file(self):
         return self.data_file.close()
 
+        
 if __name__ == "__main__":
     program = TSNE()
-    program.open_deta_file()
-
+    program.open_data_file()
+    program.put_data_into_txt()
 
 
     program.close_data_file()
