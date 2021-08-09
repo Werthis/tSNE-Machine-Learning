46bdbc94c0847f1d5228c0392e64a4be6e72cecb
Janko Walski <lukaszstawecki@gmail.com>
2021-08-09 23:13:20 +0200

    VIP #1

--- tsne.py
+++ tsne.py
@@ -6,6 +6,8 @@ class TSNE():
 
     def __init__(self):
         self.all_lists_with_splited_strings = []
+        self.all_distances_sorted = []
+        self.variances_list = []
 
     def open_data_file(self):
         self.data_file = open('probnik.csv', 'r')
@@ -19,22 +21,48 @@ class TSNE():
         for row in self.data_file_read:
             for i in row:
                 single_number_list = i.split(" ")
-                only_distances_list = single_number_list[6:]
-                self.all_lists_with_splited_strings.append(only_distances_list)
+                self.only_distances_list = single_number_list[6:]
+                self.all_lists_with_splited_strings.append(self.only_distances_list)
         # print(self.all_lists_with_splited_strings)
+        # print(len(self.only_distances_list))
+
+    def iterate(self):
+        for i in range(len(self.only_distances_list) - 1):
+            for j in self.all_lists_with_splited_strings:
+                self.all_distances_sorted.append(float(j[i]))
+            # self.all_distances_sorted.append('|||||||||||||')
+            single_variance = np.var(self.all_distances_sorted)
+            self.variances_list.append(single_variance)
+        print(self.variances_list)
+        
 
     def make_array(self):
         arr = np.array(self.all_lists_with_splited_strings)
-        print(arr)
+        # print(arr)
         return arr
 
     def put_data_into_txt(self):
-        self.file_name = 'data_300K_txt.txt'
-        self.world_map_file_1 = open(self.file_name, 'w')
-        self.world_map_file_1.write(str(self.data_read))
+        self.file_1 = 'data_300K_txt.txt'
+        self.world_map_file_1 = open(self.file_1, 'w')
+        self.world_map_file_1.write(str(self.data_file_read))
         self.world_map_file_1.close()
 
+        self.file_2 = 'all_lists_with_splited_strings.txt'
+        self.world_map_file_1 = open(self.file_2, 'w')
+        self.world_map_file_1.write(str(self.all_lists_with_splited_strings))
+        self.world_map_file_1.close()
 
+        self.file_3 = 'iterowanie.txt'
+        self.world_map_file_1 = open(self.file_3, 'w')
+        self.world_map_file_1.write(str(self.all_distances_sorted))
+        self.world_map_file_1.close()
+
+        self.file_4 = 'variances_list.txt'
+        self.world_map_file_1 = open(self.file_4, 'w')
+        self.world_map_file_1.write(str(self.variances_list))
+        self.world_map_file_1.close()
+
+        
 
     def close_data_file(self):
         return self.data_file.close()
@@ -45,7 +73,10 @@ if __name__ == "__main__":
     program.open_data_file()
     program.read_data_file()
     program.get_one_row()
-    program.make_array()
 
+    program.iterate()
+
+    program.make_array()
+    program.put_data_into_txt()
 
     program.close_data_file()
